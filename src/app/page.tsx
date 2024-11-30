2338b27f82b5439c4d87544b45a5503bcc932655
Kauê de Magalhães <kauedemagalhaes27@gmail.com>
2024-11-30 18:33:43 -0300

    feat(layout): implement header component and integrate into home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import Header from "@/components/Header";
+
 export default function Home() {
-  return <h1 className="text-3xl">Hello Portfolio</h1>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header className="max-w-7xl mx-auto px-4 py-6 flex justify-between items-center">
+      <Link href="/" className="text-xl font-medium">
+        Kauê de Magalhães
+      </Link>
+      <nav>
+        <ul className="flex space-x-8">
+          <li>
+            <Link href="/" className="text-muted-foreground hover:text-foreground">
+              Work
+            </Link>
+          </li>
+          <li>
+            <Link href="/about" className="text-muted-foreground hover:text-foreground">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link href="/resume" className="text-muted-foreground hover:text-foreground">
+              Resume
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
