04af51b1bebafa863163eb3c8d5d8e3dc1f6a9d9
Kauê de Magalhães <kauedemagalhaes27@gmail.com>
2024-11-30 18:40:44 -0300

    feat(layout): introduce Content component to enhance page structure
    • Add Content component to src/components/Content.tsx
    • Integrate Content component into main page layout in src/app/page.tsx
    • Improve overall page structure and prepare for future content additions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Content from "@/components/Content";
 import Header from "@/components/Header";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Content />
     </>
   );
 }
---
+++ src/components/Content.tsx
@@ -0,0 +1,7 @@
+export default function Content() {
+  return (
+    <main className="max-w-7xl mx-auto px-4 py-6">
+      <h1 className="text-2xl font-medium">Content</h1>
+    </main>
+  );
+}
