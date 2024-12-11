d2dd42f68984267b07ef24d8f29347651666937d
Kauê de Magalhães <kauedemagalhaes27@gmail.com>
2024-12-11 12:31:39 -0300

    feat(NavLink): enhance with active state styling

--- src/components/NavLink.tsx
+++ src/components/NavLink.tsx
@@ -1,13 +1,24 @@
+'use client'
+
 import type { Url } from 'next/dist/shared/lib/router/router'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 export default function NavLink({
   href,
   children,
 }: { href: Url; children: React.ReactNode }) {
+  const pathname = usePathname()
+  const isActive = pathname === href
+
   return (
     <li>
-      <Link href={href} className="text-muted-foreground hover:text-foreground">
+      <Link
+        href={href}
+        className={`${
+          isActive ? 'text-foreground' : 'text-muted-foreground'
+        } hover:text-foreground`}
+      >
         {children}
       </Link>
     </li>
